020b647c36a82d244da51b4a9053779de3e6ce77
Sui Gn <natsynapses@gmail.com>
2023-12-01 01:28:03 -0600

    Push
    Push

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,82 +2,75 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'suign', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
-    limit: 10, // How many projects to display.
+    limit: 4, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
       projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
-    linkedin: 'ariful-alam',
-    twitter: 'arif_szn',
-    mastodon: 'arifszn@mastodon.social',
-    facebook: '',
-    instagram: '',
-    youtube: '', // example: 'pewdiepie'
-    dribbble: '',
-    behance: '',
-    medium: 'arifszn',
-    dev: 'arifszn',
-    stackoverflow: '', // example: '1/jeff-atwood'
-    skype: '',
-    telegram: '',
-    website: 'https://arifszn.com',
-    phone: '',
-    email: 'arifulalamszn@gmail.com',
+    linkedin: 'suign',
+    instagram: 'jabella.e',
+    medium: 'suign',
+    website: 'https://neurons.me'
   },
   resume: {
     fileUrl:
-      'https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf', // Empty fileUrl will hide the `Download Resume` button.
+      '', // Empty fileUrl will hide the `Download Resume` button.
   },
   skills: [
-    'PHP',
-    'Laravel',
     'JavaScript',
-    'React.js',
+    'TypeScript',
+    'ECMAScript',
+    'React',
+    'React Native',
     'Node.js',
-    'Nest.js',
-    'MySQL',
-    'PostgreSQL',
-    'Git',
-    'Docker',
-    'PHPUnit',
-    'CSS',
-    'Antd',
-    'Tailwind',
+    'Next.js'
   ],
   experiences: [
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'September 2021',
+      company: 'Flaunt Magazine',
+      position: 'IT & Accounting',
+      from: 'September 2014',
       to: 'Present',
-      companyLink: 'https://example.com',
+      companyLink: 'https://flauntmagazine.com',
     },
     {
-      company: 'Company Name',
-      position: 'Position',
-      from: 'July 2019',
-      to: 'August 2021',
-      companyLink: 'https://example.com',
-    },
+      company: 'neurons.me',
+      position: 'Developer',
+      from: 'July 2012',
+      to: 'Present',
+      companyLink: 'https://neurons.me',
+    }
   ],
-  /* certifications: [
+  certifications: [
     {
-      name: 'Lorem ipsum',
-      body: 'Lorem ipsum dolor sit amet',
-      year: 'March 2022',
-      link: 'https://example.com'
+      name: 'Stanford University Online',
+      body: 'Machine Learning',
+      year: 'March 22 2022',
+      link: 'https://www.coursera.org/account/accomplishments/certificate/M5N8KUD9UT53'
     },
-  ], */
+    {
+      name: 'Learn C Course',
+      body: 'Codecademy',
+      year: 'March 2023',
+      link: 'https://www.codecademy.com/profiles/suiGn/certificates/8c3029c4a6e5894e74da756e3a7c0ae3'
+    },
+    {
+      name: 'Blockchain with Python Course',
+      body: 'Codecademy',
+      year: 'March 2023',
+      link: 'https://www.codecademy.com/profiles/suiGn/certificates/f21a464d190cb43e78b83ca8d1f0c6b0'
+    }
+  ], 
   education: [
     {
-      institution: 'Institution Name',
-      degree: 'Degree',
-      from: '2015',
-      to: '2019',
+      institution: 'Escuela de Aviación México',
+      degree: 'Piloto Helicoptero Privado',
+      from: '2012',
+      to: '2013',
     },
     {
       institution: 'Institution Name',
@@ -86,18 +79,18 @@ const config = {
       to: '2014',
     },
   ],
-
+ 
   // To hide the `My Projects` section, keep it empty.
   externalProjects: [
     {
-      title: 'Project Name',
+      title: 'FullTrailer',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
       imageUrl: 'https://via.placeholder.com/250x250',
       link: 'https://example.com',
     },
     {
-      title: 'Project Name',
+      title: 'OrgBoat',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut.',
       imageUrl: 'https://via.placeholder.com/250x250',
@@ -106,31 +99,27 @@ const config = {
   ],
   // Display blog posts from your medium or dev account. (Optional)
   blog: {
-    source: 'dev', // medium | dev
-    username: 'arifszn', // to hide blog section, keep it empty
-    limit: 2, // How many posts to display. Max is 10.
+    source: 'medium', // medium | dev
+    username: 'suign', // to hide blog section, keep it empty
+    limit: 4, // How many posts to display. Max is 10.
   },
   googleAnalytics: {
-    id: '', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
+    id: 'G-TQPLSH796Z', // GA3 tracking id/GA4 tag id UA-XXXXXXXXX-X | G-XXXXXXXXXX
   },
   // Track visitor interaction and behavior. https://www.hotjar.com
-  hotjar: {
-    id: '',
-    snippetVersion: 6,
-  },
   themeConfig: {
-    defaultTheme: 'wireframe',
+    defaultTheme: 'business',
 
     // Hides the switch in the navbar
     // Useful if you want to support a single color mode
-    disableSwitch: false,
+    disableSwitch: true,
 
     // Should use the prefers-color-scheme media-query,
     // using user system preferences, instead of the hardcoded defaultTheme
-    respectPrefersColorScheme: false,
+    respectPrefersColorScheme: true,
 
     // Hide the ring in Profile picture
-    hideAvatarRing: false,
+    hideAvatarRing: true,
 
     // Available themes. To remove any theme, exclude from here.
     themes: [
@@ -179,11 +168,11 @@ const config = {
   },
 
   // Optional Footer. Supports plain text or HTML.
-  footer: `Made with <a 
-      class="text-primary" href="https://github.com/arifszn/gitprofile"
+  footer: `By <a 
+      class="text-primary" href="https://neurons.me"
       target="_blank"
       rel="noreferrer"
-    >GitProfile</a> and ❤️`,
+    >neurons</a> and ❤️ me`,
 };
 
 export default config;
